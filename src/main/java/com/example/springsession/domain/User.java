a12c42c98d27afeaaca7ec7d70bec8cef531b606
dgyim <yimdgkr@gmail.com>
2024-05-12 18:34:11 +0900

    로그인 방법 구현

---
+++ src/main/java/com/example/springsession/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.springsession.login;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "\"user\"")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String loginId;
+    private String password;
+    private String nickname;
+
+    private UserRole role;
+}
---
+++ src/main/java/com/example/springsession/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.springsession.login;
+
+public enum UserRole {
+    USER, ADMIN
+}
